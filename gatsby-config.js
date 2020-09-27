b8ae3095a051538fca321a484ba97676a836b873
Tyler Turnipseed <tyler@techturnip.us>
2020-09-27 16:35:04 -0400

    initial setup, install some dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,19 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "TechTurnip",
+    author: "Tyler Turnipseed",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    "gatsby-transformer-remark",
+  ],
 }
