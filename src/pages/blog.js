c4c50760eb7d2524d3daa635e5232bdc28ceddb8
Tyler Turnipseed <tyler@techturnip.us>
2020-09-27 17:21:19 -0400

    create react components for each of the pages

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const BlogPage = () => {
+  return (
+    <Layout>
+      <h1>Blog Page</h1>
+    </Layout>
+  )
+}
+
+export default BlogPage
