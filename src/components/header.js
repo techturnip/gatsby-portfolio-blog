d3631a59c8eff13504ea87c693c706523703c142
Tyler Turnipseed <tyler@techturnip.us>
2020-09-28 11:15:59 -0400

    adjusted styling for the header, added underline nav-link animation and gradients

--- src/components/header.js
+++ src/components/header.js
@@ -14,52 +14,68 @@ const Header = () => {
   `)
 
   const siteTitle = data.site.siteMetadata.title
-  const { header, title, navList, navItem, activeNavItem } = headerStyles
+  const {
+    container,
+    header,
+    shadowWrapper,
+    title,
+    navList,
+    navItem,
+    activeNavItem,
+  } = headerStyles
 
   return (
-    <header className={header}>
-      <h1>
-        <Link to="/" className={title}>
-          {siteTitle}
-        </Link>
-      </h1>
-      <nav>
-        <ul className={navList}>
-          <li>
-            <Link to="/" className={navItem} activeClassName={activeNavItem}>
-              Home
+    <div className={shadowWrapper}>
+      <header className={header}>
+        <div className={container}>
+          <h1>
+            <Link to="/" className={title}>
+              {siteTitle}
             </Link>
-          </li>
-          <li>
-            <Link
-              to="/about"
-              className={navItem}
-              activeClassName={activeNavItem}
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/blog"
-              className={navItem}
-              activeClassName={activeNavItem}
-            >
-              Blog
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/contact"
-              className={navItem}
-              activeClassName={activeNavItem}
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+          </h1>
+          <nav>
+            <ul className={navList}>
+              <li>
+                <Link
+                  to="/"
+                  className={navItem}
+                  activeClassName={activeNavItem}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/about"
+                  className={navItem}
+                  activeClassName={activeNavItem}
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/blog"
+                  className={navItem}
+                  activeClassName={activeNavItem}
+                >
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/contact"
+                  className={navItem}
+                  activeClassName={activeNavItem}
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
   )
 }
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,13 @@ import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import "../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
 
 const Layout = props => {
+  const { container, content } = layoutStyles
   return (
-    <div>
-      <div>
+    <div className={container}>
+      <div className={content}>
         <Header />
         {props.children}
       </div>
